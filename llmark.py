9fb388fd383ead01c645cdfb183ec9c2a14bd34a
Karotte128 <karotte128@outlook.de>
2026-07-31 14:57:21 +0200

    implement run_test_set

--- llmark.py
+++ llmark.py
@@ -4,11 +4,11 @@ from .evaluator.evaluators import Evaluator
 from .evaluator.plugin import Plugin
 
 class LLMark:
-    def __init__(self, ext_plugins: dict[str, Plugin], func: Callable[[str], str]):
+    def __init__(self, func: Callable[[str], str], ext_plugins: dict[str, Plugin] = {}):
         self._eval = Evaluator(ext_plugins)
         self._chat_func = func
 
-    def run_test(self, test: dict[str, any]) -> dict[str, any]:
+    def _run_test(self, test: dict[str, any]) -> dict[str, any]:
         if "type" not in test or "question" not in test or "options" not in test:
             raise ValueError('Test must contain "type", "question" and "options".')
 
@@ -25,8 +25,22 @@ class LLMark:
 
         return self._eval.evaluate(type, options, resp)
 
-    def load_evaluators(self, eval_sources: dict[str, str]) -> None:
-        self._eval.load_evaluators(eval_sources)
+    def run_test_set(self, test_set: dict[str, any]) -> None:
+        if "evaluators" not in test_set or "questions" not in test_set:
+            raise ValueError('Test set must contain "evaluators" and "questions".')
 
-    def unload_evaluators(self) -> None:
+        evaluators: dict[str, str] = test_set["evaluators"]
+        questions: list[any] = test_set["questions"]
+
+        if not isinstance(evaluators, dict):
+            raise ValueError('"evaluators" must be a dictionary mapping.')
+
+        if not isinstance(questions, list):
+            raise ValueError('"questions" must be a list.')
+
+        self._eval.load_evaluators(evaluators)
+
+        for question in questions:
+            self._run_test(question)
+
         self._eval.unload_evaluators()
